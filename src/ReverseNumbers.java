5506543932d61277d5ca7b7a00344bb41c885d0c
Droid-An <andrei.filippov.aaf@gmail.com>
2026-07-05 19:03:35 +0100

    implement reverse numbers java task

---
+++ src/ReverseNumbers.java
@@ -0,0 +1,28 @@
+public class ReverseNumbers {
+    public static void main(String[] args) {
+        int Number = 111110000;
+        int reversedNumber = reverseNumbers(Math.abs(Number));
+        if (Number < 0) {
+            System.out.println("-" + reversedNumber);
+        } else {
+            System.out.println(reversedNumber);
+        }
+    }
+
+    /**
+     * Accepts a positive number as an argument and returns the number in reverse
+     *
+     * @param input number to reverse
+     * @return reversed number
+     */
+    private static Integer reverseNumbers(int input) {
+        String reverse = "";
+        String stringifiedInput = Integer.toString(input);
+        for (int i = stringifiedInput.length() - 1; i >= 0; i--) {
+            reverse += stringifiedInput.charAt(i);
+        }
+
+        return Integer.parseInt(reverse);
+    }
+
+}
