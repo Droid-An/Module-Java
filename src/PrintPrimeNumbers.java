c3b706684ee1e75c4e6148d58a730ec0b4bb8549
Droid-An <andrei.filippov.aaf@gmail.com>
2026-07-17 19:19:53 +0100

    log 2 only when input is more than 2

--- src/PrintPrimeNumbers.java
+++ src/PrintPrimeNumbers.java
@@ -8,7 +8,9 @@ public class PrintPrimeNumbers {
 
     private static void findPrimeNumber(int input) {
         //2 doesn't fit in the loop, so I have to print it out separately
-        System.out.println(2);
+        if (input > 2 ){
+            System.out.println(2);
+        }
 
         for (int i = 3; i <= input; i = i + 2) {
             boolean isPrime = true;
