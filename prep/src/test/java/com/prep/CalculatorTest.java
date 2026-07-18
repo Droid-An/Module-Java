08c4ff8cf6aa7dd554cf1ff4af4b1cb537c47674
Droid-An <andrei.filippov.aaf@gmail.com>
2026-07-18 15:30:52 +0100

    use import * from assertions

--- prep/src/test/java/com/prep/CalculatorTest.java
+++ prep/src/test/java/com/prep/CalculatorTest.java
@@ -1,12 +1,8 @@
 package com.prep;
 
-import org.junit.jupiter.api.Assertions;
+import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
-
 
 class CalculatorTest {
     Calculator calculator = new Calculator();
@@ -17,22 +13,18 @@ class CalculatorTest {
     }
 
     @Test
-    void testAdd(
-
-    ) {
-        assertEquals(4, calculator.add(2,2));
+    void testAdd() {
+        assertEquals(4, calculator.add(2, 2));
     }
-// what's the difference between "Assertions.assertEquals" and just "assertEquals"?
+
     @Test
     void testDivide() {
-        Assertions.assertEquals(4, calculator.subtract(6,2));
+        assertEquals(4, calculator.subtract(6, 2));
     }
 
     @Test
-    void testDivideByZeroThrows(
-
-    ) {
-        Exception exception = assertThrows(IllegalArgumentException.class, ()-> calculator.divide(6,0));
+    void testDivideByZeroThrows() {
+        Exception exception = assertThrows(IllegalArgumentException.class, () -> calculator.divide(6, 0));
         assertEquals("Division by zero", exception.getMessage());
     }
 }
