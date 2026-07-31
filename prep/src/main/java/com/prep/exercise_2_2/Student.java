39cd063128d08957b95c475a152af2de30f3cb96
Droid-An <andrei.filippov.aaf@gmail.com>
2026-07-31 20:31:41 +0100

    reformat code

--- prep/src/main/java/com/prep/exercise_2_2/Student.java
+++ prep/src/main/java/com/prep/exercise_2_2/Student.java
@@ -2,7 +2,6 @@ package com.prep.exercise_2_2;
 
 
 public record Student(String firstName, String lastName) {
-
     @Override
     public String toString() {
         return firstName + " " + lastName;
